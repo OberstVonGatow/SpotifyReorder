e96e8043af880ff5656c2eb4a01c7ab8aa7133b1
OberstVonGatow <OberstVonGatow@users.noreply.github.com>
2020-10-25 16:24:02 +0100

    Update ReSort.py
    fixed bug in check_tracks
    changed playlistname to array
    added utf-8 decode for Playlists with special characters

--- ReSort.py
+++ ReSort.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sys
 import spotipy
 import spotipy.util as util
@@ -9,12 +10,14 @@ def check_tracks(results, first_date, counter):
         temp_date = item['added_at']
         if temp_date > first_date:
             track = item['track']
-            print( counter, track['artists'][0]['name'], track['name'] )
-            sp.user_playlist_reorder_tracks(username,playlist['id'],counter,0)
+            print(counter, track['artists'][0]['name'], track['name'])
+            sp.user_playlist_reorder_tracks(
+                username, playlist['id'], counter, 1, 0)
             sleep(0.5)
             first_date = temp_date
         counter += 1
     return counter
+
 
 if __name__ == '__main__':
 
@@ -23,32 +26,36 @@ if __name__ == '__main__':
 ##########################################
 #   EDIT HERE
     username = 'XXXXXXXX'
-    playlistname = 'XXXXXXXX'
+    playlistarray = [
+        'PLAYLISTNAME1'.decode('utf-8'),
+        'PLAYLISTNAME2'.decode('utf-8'),
+        'PLAYLISTNAME99'.decode('utf-8'),
+    ]
     client_id = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
     client_secret = 'XXXXXXXXXXXXXXXXXXXXXXXX'
 ##########################################
-    counter = 0
 
-    token = util.prompt_for_user_token(username,scope,client_id = client_id,client_secret = client_secret,redirect_uri='http://localhost/')
+    token = util.prompt_for_user_token(
+        username, scope, client_id=client_id, client_secret=client_secret, redirect_uri='http://localhost/')
     if token:
         sp = spotipy.Spotify(auth=token)
-        playlists = sp.current_user_playlists(limit=50,offset=0)
+        playlists = sp.current_user_playlists(limit=50, offset=0)
         while playlists:
             for i, playlist in enumerate(playlists['items']):
-                if playlist['owner']['id'] == username and playlist['name'] == playlistname:
-                    results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+                if playlist['owner']['id'] == username and playlist['name'] in playlistarray:
+                    results = sp.user_playlist(
+                        username, playlist['id'], fields="tracks,next")
                     first_date = results['tracks']['items'][0]['added_at']
                     tracks = results['tracks']
-                    
+                    counter = 0
                     counter = check_tracks(tracks, first_date, counter)
                     while tracks['next']:
                         tracks = sp.next(tracks)
                         counter = check_tracks(tracks, first_date, counter)
-                
+
             if playlists['next']:
                 playlists = sp.next(playlists)
             else:
-                playlists = None  
+                playlists = None
     else:
         print("Can't get token for", username)
-
